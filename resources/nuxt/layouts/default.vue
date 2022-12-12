6bbe3d378ce4361daff7582b8ef679ce24a9d204
Ibrahim Mustapha <mustophatg@gmail.com>
2022-12-12 01:37:59 -0500

    fixed mobile hamburger links

--- resources/nuxt/layouts/default.vue
+++ resources/nuxt/layouts/default.vue
@@ -1,39 +1,32 @@
 <template>
-
-    <div class="drawer">
-
+    <div class="drawer drawer-mobile">
         <input id="my-drawer" type="checkbox" class="drawer-toggle" />
 
-        <div class="drawer-content ">
-
+        <div class="drawer-content">
             <!-- header -->
-                <Header></Header>
+            <Header></Header>
             <!-- end of header -->
 
-
             <!-- Page content here -->
-                <Nuxt class="" />
+            <Nuxt class="" />
             <!-- Page content here -->
-
-        
-
         </div>
 
         <div class="drawer-side">
-
             <label for="my-drawer" class="drawer-overlay"></label>
 
-             <ul class="menu bg-base-100  lg:hidden">
-                <li class=""> <nuxt-link to="/">Home</nuxt-link> </li>
-                <li class=""> <nuxt-link to="/about">About</nuxt-link> </li>
-                <li class=""> <nuxt-link to="/experience">Experience</nuxt-link> </li>
-                <li class=""> <nuxt-link to="/projects">Projects</nuxt-link> </li>
-                <li class=""> <nuxt-link to="/contact">Contact</nuxt-link> </li>
+            <ul class="menu bg-base-100 lg:hidden">
+                <li class="menu-title mb-4">
+                    <span>Mobile Links</span>
+                </li>
+                <li class=""><nuxt-link to="/">Home</nuxt-link></li>
+                <li class=""><nuxt-link to="/about">About</nuxt-link></li>
+                <li class="">
+                    <nuxt-link to="/experience">Experience</nuxt-link>
+                </li>
+                <li class=""><nuxt-link to="/projects">Projects</nuxt-link></li>
+                <li class=""><nuxt-link to="/contact">Contact</nuxt-link></li>
             </ul>
-
         </div>
-
     </div>
-
 </template>
-
