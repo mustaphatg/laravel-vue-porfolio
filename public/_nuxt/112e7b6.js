3574e64520b6cc353e0ef94dfd58ebd8db686395
Ibrahim Mustapha <mustophatg@gmail.com>
2022-12-14 08:32:22 -0500

    changed project styles

--- public/_nuxt/112e7b6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{208:function(t,n,e){"use strict";e.r(n);var o=e(21),component=Object(o.a)({},(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("div",[t("p",[this._v("about mustapha Ibrahim, about")])])}],!1,null,null,null);n.default=component.exports}}]);
